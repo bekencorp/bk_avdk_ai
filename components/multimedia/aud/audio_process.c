ecd8b9ea5821edb0dce05a4053085399be27f06f
itester <itester@aclsemi.com>
2025-04-19 22:30:09 +0800

    [CI] [56591]  <platform> <feat>add eq function and vad info interface

--- components/multimedia/aud/audio_process.c
+++ components/multimedia/aud/audio_process.c
@@ -12,7 +12,7 @@ app_aud_para_t aud_para = {
 
 void voice_dl_process_init(void)
 {
-    //app_aud_eq_init(&aud_para.eq_dl_voice,EQ_ID_DL_VOICE);
+    app_aud_eq_init(&aud_para.eq_dl_voice,EQ_ID_DL_VOICE);
     bk_printf("voice_dl_process_init\r\n" );
 
 }
@@ -24,7 +24,7 @@ void voice_dl_process_deinit(void)
 
 void voice_dl_process(int16 *buf, uint32 sample_points)
 {
-    app_aud_eq_process(buf, sample_points, EQ_ID_DL_VOICE);
+    //app_aud_eq_process(buf, sample_points, EQ_ID_DL_VOICE);
 }
 
 bk_err_t audio_para_init(app_aud_para_t *aud_para_ptr)
