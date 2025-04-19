b1ff20ffb28efc26c625a1fd216579e429a2b79e
itester <itester@aclsemi.com>
2025-04-19 22:42:15 +0800

    [CI] [56926]  <platform> <feat> optimize eq  parameter

--- components/multimedia/include/audio_para.h
+++ components/multimedia/include/audio_para.h
@@ -10,18 +10,19 @@ extern "C" {
 ///default eq parameter
 
 #define EQ0 1
-#define EQ0A0 -1863575
-#define EQ0A1 828007
-#define EQ0B0 935039
-#define EQ0B1 -1870079
-#define EQ0B2 935039
+#define EQ0A0 -1668050
+#define EQ0A1 734106
+#define EQ0B0 934084
+#define EQ0B1 -1715174
+#define EQ0B2 801474
 
 #define EQ1 1
-#define EQ1A0 -1731569
-#define EQ1A1 737068
-#define EQ1B0 982959
-#define EQ1B1 -1747842
-#define EQ1B2 786411
+#define EQ1A0 -1764715
+#define EQ1A1 784980
+#define EQ1B0 1034243
+#define EQ1B1 -1764715
+#define EQ1B2 799312
+
 
 #define FILTER_PREGAIN_FRA_BITS (14)
 
