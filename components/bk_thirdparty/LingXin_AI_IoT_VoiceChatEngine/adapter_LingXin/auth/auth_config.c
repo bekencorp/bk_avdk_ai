409857bd30f5e36775442af0a0bcede69ca82cfd
itester <itester@aclsemi.com>
2025-07-11 19:34:04 +0800

    [CI] [65202]  <wifi> <feat>update lingxin thirdparty

---
+++ components/bk_thirdparty/LingXin_AI_IoT_VoiceChatEngine/adapter_LingXin/auth/auth_config.c
@@ -0,0 +1,18 @@
+#include "lingxin_auth_config.h"
+
+char *lingxin_auth_appId_get()
+{
+    return "xxx";
+}
+char *lingxin_auth_appKey_get()
+{
+    return "xxx";
+}
+char *lingxin_auth_sn_get()
+{
+    return "xxx";
+}
+char *lingxin_auth_agentCode_get()
+{
+    return "xxx";
+}
--- components/bk_thirdparty/LingXin_AI_IoT_VoiceChatEngine/chat_include/chat_system_include.h
+++
@@ -1,7 +0,0 @@
-// bool char等类型引入
-
-// #include "system/includes.h"
-
-#include <stdio.h>
-#include <stddef.h>
-#include <stdbool.h>
