0bd9357937dcb80188b71515570b51555717a58e
itester <itester@aclsemi.com>
2025-07-22 11:10:05 +0800

    [CI] [65829]  <wifi> <fix> add auth management for Lingxin

--- components/bk_thirdparty/LingXin_AI_IoT_VoiceChatEngine/adapter_LingXin/auth/auth_config.c
+++ components/bk_thirdparty/LingXin_AI_IoT_VoiceChatEngine/adapter_LingXin/auth/auth_config.c
@@ -1,5 +1,26 @@
-#include "lingxin_auth_config.h"
+#include "auth_config.h"
 
+#if !CONFIG_ENABLE_LINGXIN_COSTOM_AUTH
+struct lingxin_auth_config auth_config = {0};
+
+char *lingxin_auth_appId_get()
+{
+    return auth_config.appId;
+}
+char *lingxin_auth_appKey_get()
+{
+    return auth_config.appKey;
+}
+char *lingxin_auth_sn_get()
+{
+    return auth_config.sn;
+}
+char *lingxin_auth_agentCode_get()
+{
+    return auth_config.agentCode;
+}
+#else
+/*customers need to config appid, appkey, sn, agentcode follow Lingxin Docs*/
 char *lingxin_auth_appId_get()
 {
     return "xxx";
@@ -16,3 +37,4 @@ char *lingxin_auth_agentCode_get()
 {
     return "xxx";
 }
+#endif
