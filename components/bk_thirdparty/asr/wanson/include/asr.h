7d1139d60c14b047bff58140c807ed84f9fe91e9
itester <itester@aclsemi.com>
2025-10-15 17:40:36 +0800

    [CI] [72496]  <multimedia> <feat> Authorize the user with cn Wanson.

--- components/bk_thirdparty/asr/wanson/include/asr.h
+++ components/bk_thirdparty/asr/wanson/include/asr.h
@@ -13,9 +13,12 @@ extern "C" {
 #include "fst_graph.h"
 #include "fst_words.h"
 #endif
-
 // Return value   : 0 - OK, -1 - Error
+#if (CONFIG_WANSON_CN_LICENSE)
+int  Wanson_ASR_Init(uint8_t userid[8]); 
+#else
 int  Wanson_ASR_Init();
+#endif
 
 #if(CONFIG_WANSON_ASR_GROUP_VERSION)
 void Wanson_ASR_Set_Fst(Fst *fst);
@@ -24,15 +27,15 @@ void Wanson_ASR_Reset();
 
 /*****************************************
 * Input:
-*      - buf      : Audio data (16k, 16bit, mono) 
-*      - buf_len  : Now must be 480 (30ms) 
+*      - buf      : Audio data (16k, 16bit, mono)
+*      - buf_len  : Now must be 480 (30ms)
 *
 * Output:
-*      - text     : The text of ASR 
+*      - text     : The text of ASR
 *      - score    : The confidence of ASR (Now not used)
 *
 * Return value    :  0 - No result
-*                    1 - Has result 
+*                    1 - Has result
 *                   -1 - Error
 ******************************************/
 int  Wanson_ASR_Recog(short *buf, int buf_len, const char **text, float *score);
